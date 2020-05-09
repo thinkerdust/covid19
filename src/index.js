1c0483e8976f5dd582ae98a0cdf9eaeb0ebda428
thinkerdust <ayurakhma1@gmail.com>
2020-05-09 14:37:59 +0700

    content web info covid

--- src/index.js
+++ src/index.js
@@ -1,7 +1,14 @@
 import "jquery/dist/jquery.min.js"
 import "bootstrap/dist/css/bootstrap.min.css";
-import "bootstrap/dist/js/bootstrap.min.js"
+import "bootstrap/dist/js/bootstrap.min.js";
 import "./css/style.css";
 import main from "./js/main.js";
+import washhand from './img/1.png';
+import usemask from './img/2.png';
+import scldistancing from './img/3.png';
+
+document.querySelector('#WashHand').src = washhand;
+document.querySelector('#UseMask').src = usemask;
+document.querySelector('#SclDistancing').src = scldistancing;
 
 document.addEventListener("DOMContentLoaded", main);
--- src/js/main.js
+++ src/js/main.js
@@ -1,36 +1,45 @@
 function main() {
-    const getdatacovidIndo = () => {
-        fetch('https://indonesia-covid-19.mathdro.id/api').then(response => {
+    const getdatacovid = () => {
+        fetch("https://indonesia-covid-19.mathdro.id/api")
+        .then(response => {
             return response.json();
         })
         .then(responseJson => {
             if(responseJson.error) {
                 showResponseMessage(responseJson.message);
             } else {
-                renderAlldata(responseJson.indo);
+                renderAllDataCovid(responseJson);
             }
-            // document.body.innerText = JSON.stringify(responseJson);
         })
         .catch(error => {
             showResponseMessage(error);
-        })
+        });
+    };
+
+    const renderAllDataCovid = (responseJson) => {
+        const {meninggal, sembuh, perawatan, jumlahKasus} = responseJson;
+        const datapositif = document.querySelector("#indopositif");
+        const datadirawat = document.querySelector("#indodirawat");
+        const datasembuh = document.querySelector("#indosembuh");
+        const datameninggal = document.querySelector("#indomeninggal");
+
+        datapositif.innerHTML = '<h1 class="card-title pricing-card-title">' + jumlahKasus +'</h1>';
+
+        datadirawat.innerHTML = '<h1 class="card-title pricing-card-title">' + perawatan +'</h1>';
+
+        datasembuh.innerHTML = '<h1 class="card-title pricing-card-title">' + sembuh +'</h1>';
+
+        datameninggal.innerHTML = '<h1 class="card-title pricing-card-title">' + meninggal +'</h1>';
+
     };
 
     const showResponseMessage = (message = "Check your internet connection") => {
         alert(message);
     };
 
-    const renderAlldata = (indo) => {
-        const listIndopositifElement =  document.querySelector("#indopositif");
-        listIndopositifElement.innerHTML += '<h1 class="card-title pricing-card-title">10</h1>';
-        
-    };
-
     document.addEventListener("DOMContentLoaded", () => {
-        getdatacovidIndo();
+        getdatacovid();
     });
-
-
 }
 
 export default main();
